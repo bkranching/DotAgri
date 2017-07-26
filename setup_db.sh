87df22e8c59fc55feebbead47aeac3da8e00329f
verax-wares <verax@8chan.co>
2017-07-26 00:50:06 -0400

    Fix quoting in db setup script

--- setup_db.sh
+++ setup_db.sh
@@ -5,13 +5,13 @@
 # You probably want to run this script as the postgres superuser - pgsql or whatever user your OS sets up.
 # This should be the same user you ran 'initdb' as.
 
-[ -z $1 ] && echo "Usage: $0 password [-f]\n\tThe password is for setting up the regnum user" && exit
+[ -z "$1" ] && echo "Usage: $0 password [-f]\n\tThe password is for setting up the regnum user" && exit
 
-count=$(psql -l | wc -l | tr -d ' ')
-retval=$?
+count="$(psql -l | wc -l | tr -d ' ')"
+retval="$?"
 
-[ $count -gt 0 -a $2 != '-f' ] && echo "Error: There are already databases in postgres.  If you're sure you want to continue anyways, add a -f flag to this command" && exit
-[ $retval != 0 ] && echo "Error from psql" && exit
+[ "$count" -gt 0 -a "$2" != '-f' ] && echo "Error: There are already databases in postgres.  If you're sure you want to continue anyways, add a -f flag to this command" && exit
+[ "$retval" != 0 ] && echo "Error from psql" && exit
 
 regnum_dir=${0%/*}
 echo "Setting up regnum DB..."
